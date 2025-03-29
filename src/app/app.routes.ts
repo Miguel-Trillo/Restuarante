a4fc7fec4de1e80e5f10453961790bdd2278e87f
Miguel Trillo <migueltrillo1903@gmail.com>
2025-03-29 00:26:54 -0500

    Se añadió el diseño base del login, del menú y de la sección de selección de mesas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { MesasComponent } from './mesas/mesas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MesasComponent },
+];
