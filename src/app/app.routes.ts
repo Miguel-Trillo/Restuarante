4cd6f142ea8e3d2f15048f74ca5e1352d5edf32f
Miguel Trillo <migueltrillo1903@gmail.com>
2025-03-30 00:42:33 -0500

    Se edito el login, la seccion de mesas y se añadio el componente donde apareceran las tablas de administracion de pedidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { MesasComponent } from './mesas/mesas.component';
+import { AdminPedidosComponent } from './admin-pedidos/admin-pedidos.component';
 
 export const routes: Routes = [
-    { path: '', component: MesasComponent },
+    { path: '', component: LoginComponent },
 ];
