551901df69e10fa95aa573e977d8d139805e59e8
Miguel Trillo <migueltrillo1903@gmail.com>
2025-03-30 20:44:28 -0500

    Se añadio la tabla de pedidos pendientes

--- src/app/admin-pedidos/admin-pedidos.component.ts
+++ src/app/admin-pedidos/admin-pedidos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TablaComponent } from '../tabla/tabla.component';
 
 @Component({
   selector: 'app-admin-pedidos',
-  imports: [],
+  imports: [TablaComponent],
   templateUrl: './admin-pedidos.component.html',
   styleUrl: './admin-pedidos.component.css'
 })
